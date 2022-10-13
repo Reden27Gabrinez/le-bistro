99aa2bd120b7833124025d8282460b3d991304b4
Reden27Gabrinez <redengabrinez1@gmail.com>
2022-10-13 17:45:32 +0800

    add reports

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -21,6 +21,108 @@ else
     <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
     <link href="css/helper.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
+
+    <style>
+        :root {
+  --modal-duration: 1s;
+  --modal-color: #428bca;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  background: #f4f4f4;
+  font-size: 17px;
+  line-height: 1.6;
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+}
+
+.button {
+  background: #428bca;
+  padding: 1em 2em;
+  color: #fff;
+  border: 0;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.button:hover {
+  background: #3876ac;
+}
+
+.modal {
+  display: none;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-content {
+  margin: 10% auto;
+  width: 60%;
+  box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2), 0 7px 20px 0 rgba(0, 0, 0, 0.17);
+  animation-name: modalopen;
+  animation-duration: var(--modal-duration);
+}
+
+.modal-header h2,
+.modal-footer h3 {
+  margin: 0;
+}
+
+.modal-header {
+  background: var(--modal-color);
+  padding: 15px;
+  color: #fff;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+
+.modal-body {
+  padding: 10px 20px;
+  background: #fff;
+}
+
+.modal-footer {
+  background: var(--modal-color);
+  padding: 10px;
+  color: #fff;
+  text-align: center;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+}
+
+.close {
+  color: #ccc;
+  float: right;
+  font-size: 30px;
+  color: #fff;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+@keyframes modalopen {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+    </style>
 </head>
 
 <body class="fix-header">
@@ -92,8 +194,10 @@ else
                         </li>
 						 <!-- <li> <a href="all_orders.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Orders</span></a></li> -->
                          <li> <a href="reservation.php"><i class="fa fa-table" aria-hidden="true"></i><span>Reservation</span></a></li>
+                         <li> <a id="modal-btn"><i class="fa fa-bar-chart" aria-hidden="true"></i><span>Reports</span></a></li>
+                            <?php include("reports_modals.php"); ?>
                          <li> <a href="table.php"><i class="fa fa-table" aria-hidden="true"></i><span>Table</span></a></li>
-                         <li> <a href="email/backupCloud.php"><i class="fa fa-database" aria-hidden="true"></i><span>Back-up Database</span></a></li>
+                         <li> <a href="backup/backupCloud.php"><i class="fa fa-database" aria-hidden="true"></i><span>Back-up Database</span></a></li>
                          
                     </ul>
                 </nav>
